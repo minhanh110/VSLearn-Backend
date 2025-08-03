e945751e70e701d10cac561f9431dfb30fb5d228
DucAnh <156211578+QuocPMHE172252@users.noreply.github.com>
2025-08-04 06:32:13 +0700

    update pricing entity

--- VSLearn/src/main/java/com/vslearn/entities/Pricing.java
+++ VSLearn/src/main/java/com/vslearn/entities/Pricing.java
@@ -2,6 +2,8 @@ package com.vslearn.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -19,6 +21,7 @@ import java.time.Instant;
 @Table(name = "pricing")
 public class Pricing {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", columnDefinition = "int UNSIGNED not null")
     private Long id;
 
@@ -60,10 +63,6 @@ public class Pricing {
     @Column(name = "deleted_by", columnDefinition = "int UNSIGNED")
     private Long deletedBy;
 
-/*
- TODO [Reverse Engineering] create field to map the 'discount_percent' column
- Available actions: Define target Java type | Uncomment as is | Remove column mapping
-    @Column(name = "discount_percent", columnDefinition = "double UNSIGNED not null")
-    private Object discountPercent;
-*/
+    @Column(name = "is_active", columnDefinition = "boolean not null default true")
+    private Boolean isActive = true;
 }
