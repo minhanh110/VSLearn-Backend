320dcef83ef0b78adaa4d03d15e0d14d40aa9575
Quocpm <156211578+QuocPMHE172252@users.noreply.github.com>
2025-08-07 19:18:39 +0700

    add notify entity

---
+++ VSLearn/src/main/java/com/vslearn/entities/Notify.java
@@ -0,0 +1,67 @@
+package com.vslearn.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "notify", indexes = {
+        @Index(name = "idx_notify_to_user", columnList = "to_user_id"),
+        @Index(name = "idx_notify_is_send", columnList = "is_send"),
+        @Index(name = "idx_notify_created_at", columnList = "created_at")
+})
+public class Notify {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "int UNSIGNED not null")
+    private Long id;
+
+    @NotNull
+    @Lob
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "from_user_id", nullable = false)
+    private User fromUser;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "to_user_id", nullable = false)
+    private User toUser;
+
+    @NotNull
+    @Column(name = "is_send", nullable = false)
+    private Boolean isSend = false;
+
+    @NotNull
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "created_by", columnDefinition = "int UNSIGNED not null")
+    private Long createdBy;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "updated_by", columnDefinition = "int UNSIGNED")
+    private Long updatedBy;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+
+    @Column(name = "deleted_by", columnDefinition = "int UNSIGNED")
+    private Long deletedBy;
+} 
